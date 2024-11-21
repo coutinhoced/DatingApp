76fe827892ed31e5bd37671ab109fff0eb36f507
coutinhoced <86143076+coutinhoced@users.noreply.github.com>
2024-11-21 17:11:12 +0530

    Implemented User persistence.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,22 +1,26 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavComponent } from "./nav/nav.component";
+import { AccountService } from './_services/account.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
  
   http = inject(HttpClient);
-  title = 'Dating Application';
+  private accountService = inject(AccountService);
+  title = 'Love';
   users: any;
 
   ngOnInit(): void {
-    this.http.post("https://localhost:7032/api/User/GetUsers", {}).subscribe(
+    this.setCurrentUser();
+   /*  this.http.post("https://localhost:7032/api/User/GetUsers", {}).subscribe(
       {
         next:(response)=>{ 
           this.users = response
@@ -24,7 +28,16 @@ export class AppComponent implements OnInit {
         error:(error) =>{console.log(error)},
         complete:()=>{console.log(this.users)}
       }
-    )
+    ) */
+  }
+
+  setCurrentUser(){
+    const userString = localStorage.getItem('user');
+    if(!userString) return;
+
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+
   }
 
 }
